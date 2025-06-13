a4fd9a15817c856101c9e28c2e632abef5f25118
angie <angie.guemes@mongodb.com>
2025-06-13 15:01:44 -0600

    start details tabs

--- frontend/components/businessIntelligence/BusinessIntelligenceContainer.jsx
+++ frontend/components/businessIntelligence/BusinessIntelligenceContainer.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import './businessIntelligenceContainer.css';
 import { Description } from '@leafygreen-ui/typography';
 import Icon from '@leafygreen-ui/icon';
+import Card from '@leafygreen-ui/card';
 
 const BusinessIntelligenceContainer = (props) => {
     const { } = props;
 
     return (
         <div className='mt-4 row'>
-           BI 
+           <Card>
+                a
+            </Card> 
+                       <Card>
+                a
+            </Card> 
+                       <Card>
+                a
+            </Card> 
         </div>
     )
 }
--- frontend/components/locationsContainer/LocationsContainer.jsx
+++ frontend/components/locationsContainer/LocationsContainer.jsx
@@ -2,13 +2,52 @@ import React from 'react'
 import './locationsContainer.css';
 import { Description } from '@leafygreen-ui/typography';
 import Icon from '@leafygreen-ui/icon';
+import Card from '@leafygreen-ui/card';
 
 const LocationsContainer = (props) => {
     const { } = props;
 
     return (
         <div className='mt-4 row'>
-           Loc
+            <Card className='mb-4'>
+                <p className='medium-text text-dark'>
+                    <Icon glyph="Package" size="small" /> {/* Aisle icon */}
+                    <strong>Shelf Inventory</strong>
+                </p>
+                <div className='row'>
+                    <div  className='col-4 col-md-4 col-sm-12 col-12 mb-4 d-flex flex-column align-items-center'>
+
+                        <p className='m-0 medium-text text-dark'>Aisle:</p>
+                        <p className='mb-0'>D3</p>
+
+                    </div>
+                    <div  className='col-4 col-md-4 col-sm-12 col-12 mb-4 d-flex flex-column align-items-center'>
+
+                        <p className='m-0 medium-text text-dark'>Section:</p>
+                        <p className='mb-0'>Dairy</p>
+
+                    </div>
+                    <div  className='col-4 col-md-4 col-sm-12 col-12 mb-4 d-flex flex-column align-items-center'>
+
+                        <p className='m-0 medium-text text-dark'>Aisle:</p>
+                        <p className='mb-0'>Middle</p>
+
+                    </div>
+                </div>
+            </Card>
+            <Card className='mb-4 '>
+                <p className='medium-text text-dark mb-1'>
+                    <Icon glyph="MagnifyingGlass" size="small" /> {/* Aisle icon */}
+                    <strong>Store Map</strong>
+                </p>
+                <Description className='medium-text mt-0'>See product location on the store map</Description>
+            </Card>
+            <Card className='mb-4'>
+                <p className='medium-text text-dark'>
+                    <Icon glyph="Building" size="small" /> {/* Aisle icon */}
+                    <strong>Other Store Availability</strong>
+                </p>
+            </Card>
         </div>
     )
 }
